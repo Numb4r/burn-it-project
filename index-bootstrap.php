3ff4901cb5f59686af112d5bbbe5587c23a1eff6
Marcos Vinicius <juninhowblu@gmail.com>
2016-06-25 21:59:28 -0300

    Adicionando suporte ao bootstrap
    Index-bootstrap = exemplo.

---
+++ index-bootstrap.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: MVMCJ
+ * Date: 24/06/2016
+ * Time: 23:30
+ */
+
+?>
+
+<head>
+  <link rel="stylesheet" href="bootstrap/content/bootstrap.css">
+  <link rel="stylesheet" href="thether/Content/tether/tether.css">
+  <link rel="stylesheet" href="semantic/dist/components/transition.css">
+  <link rel="stylesheet" href="scripts/jumbotron.css">
+
+</head>
+<body>
+
+<div class="bg"></div>
+<div class="jumbotron">
+  <h1>Bootstrap Jumbotron</h1>
+  <p class="lead">+ Parallax Effect using jQuery</p>
+</div>
+
+<div class="container">
+  <div class="row">
+    <button id="xd" class="btn btn-secondary-outline">ASDASD</button>
+    <img class="leaf" src="http://previews.123rf.com/images/nujalee/nujalee1108/nujalee110800004/10410227-Beautiful-green-leaf-isolated-on-white-Stock-Photo-leaf-texture.jpg"/>
+  </div>
+</div>
+
+<hr>
+
+<div class="container">
+  <div class="row">
+    <div class="col-md-12 text-center"><p>The End.</p></div>
+  </div>
+</div>
+
+<script src="jquery.min.js"></script>
+<script src="thether/Scripts/tether/tether.js"></script>
+<script src="bootstrap/scripts/bootstrap.js"></script>
+<script src="semantic/dist/semantic.css"></script>
+<script src="semantic/dist/components/transition.js"></script>
+
+<script src="scripts/init.js"></script>
+</body>
